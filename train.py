140b22274ecf289ffb97a1c7ef18465f48e4a146
lougriz <miguelvalente@protonmail.com>
2021-06-08 14:56:29 +0200

    conda yaml

--- train.py
+++ train.py
@@ -27,31 +27,14 @@ run = wandb.init(project='toy_data_zf', entity='mvalente',
 config = wandb.config
 
 points_per_sample = 30000
-run.config['points_per_sample'] = points_per_sample
-
-toy_data = ToyData(points_per_sample, show_data=False)
-generate_all = True
-
-
-cs = toy_data.cs
-cu = toy_data.cu
-contexts = torch.vstack((cs, cu))
-
-cs = cs.to(device)
-cu = cu.to(device)
-contexts = contexts.to(device)
 
 input_dim = 4
 context_dim = 2
 split_dim = input_dim - context_dim
 
-train_loader = DataLoader(toy_data,
+train_loader = DataLoader(,
                           batch_size=config['batch_size'],
                           shuffle=False, pin_memory=True)
-
-test_seen = toy_data.x.clone()
-test_seen = [t for t in torch.split(test_seen,
-                                    points_per_sample)]
 
 
 visual_distribution = dist.MultivariateNormal(torch.zeros(split_dim).to(device), torch.eye(split_dim).to(device))
@@ -92,7 +75,6 @@ optimizer = optim.Adam(flow.parameters(), lr=config['lr'])
 
 epochs = tqdm.trange(1, config['epochs'])
 
-number_samples = 400
 for epoch in epochs:
     losses = []
     losses_flow = []
@@ -114,28 +96,6 @@ for epoch in epochs:
         losses_centr.append(centralizing_loss.item())
         losses_mmd.append(mmd_loss.item())
         losses.append(loss.item())
-
-    if True:
-        with torch.no_grad():
-            test_data = []
-            if generate_all:
-                for c_id, c in enumerate(contexts):
-                    test_data.append(flow.generation(
-                                     torch.hstack((c.repeat(number_samples).reshape(-1, 2),
-                                                  flow.base_dist.visual_distribution.sample([number_samples])))))
-
-                test_data = [data.to("cpu").detach().numpy() for data in test_data]
-                make_toy_graph(test_data, epoch, "all generated", save=True)
-
-            else:
-                test_data = [data[:number_samples] for data in test_seen]
-                test_data.append(flow.generation(
-                                 torch.hstack((cu.repeat(number_samples).reshape(-1, 2),
-                                               flow.base_dist.visual_distribution.sample([number_samples])))))
-
-                test_data = [data.to("cpu").detach().numpy() for data in test_data]
-
-                make_toy_graph(test_data, epoch, "unseen generated", save=True)
 
     run.log({"loss": sum(losses) / len(losses),
              "loss_flow": sum(losses_flow) / len(losses_flow),  # }, step=epoch)
